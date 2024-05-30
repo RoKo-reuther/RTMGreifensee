c86f3a474d9352621c573410d9540f8271c754d7
RoKo-reuther <blubberbob22222@gmail.com>
2024-05-30 12:28:29 +0200

    Remove .onLoad & .onUnload and replace with automatic SHLIB loadingthrough NAMESPACE

--- R/compile_and_load.R
+++ R/compile_and_load.R
@@ -1,22 +1,23 @@
-# THIS IS AUTOMATICALLY DONE BY PACKAGE INSTALLER and .onLoad
-# BUT IT CAN SHOW HOW TO COMPILE AND LOAD A MODEL MANUALLY
+#' @useDynLib RTMGreifensee
+
+# Compilation of the model is automatically done during the package installation.
+# Loading the shared libraries is automatically done when the package is loaded.
+# The following functions just display how compilation and loading could be done manually.
 
 # ==============================================================================
 # > Compile Model
 # ==============================================================================
 compile_model <- function() {
 
-    # !!! make it OS agnostic !!!
+    lib_path <- "your_lib_path"
+    shlib    <- paste0(lib_path, "/RTMGreifensee", .Platform$dynlib.ext)
 
-    prefix <- paste0(system.file("fortran", package = "RTMGreifensee"), "/")
-
-    shlib  <- paste0(prefix, "RTMGreifensee", .Platform$dynlib.ext)
-
-    model    <- paste0(prefix, "model.f95")
-    routines <- paste0(prefix, "setup_routines.f95")
-    tran1d   <- paste0(prefix, "tran1d.f95")
-    tableau  <- paste0(prefix, "solve_tableau.f95")
-    dgesv    <- paste0(prefix, "dgesv.f")
+    src_path <- "your_source_path"
+    model    <- paste0(src_path, "/model.f95")
+    routines <- paste0(src_path, "/setup_routines.f95")
+    tran1d   <- paste0(src_path, "/tran1d.f95")
+    tableau  <- paste0(src_path, "/solve_tableau.f95")
+    dgesv    <- paste0(src_path, "/dgesv.f")
 
     command <- paste0(
         "R CMD SHLIB -c -o ", shlib, " ",
@@ -37,7 +38,8 @@ compile_model <- function() {
 # ==============================================================================
 load_model <- function() {
 
-    shlib <- system.file(paste0("fortran/RTMGreifensee", .Platform$dynlib.ext), package = "RTMGreifensee")
+    lib_path <- "your_lib_path"
+    shlib    <- paste0(lib_path, "/RTMGreifensee", .Platform$dynlib.ext)
 
     if (is.loaded("derivs")) {
         dyn.unload(shlib)
@@ -49,7 +51,8 @@ load_model <- function() {
 
 unload_model <- function() {
 
-    shlib <- system.file(paste0("fortran/RTMGreifensee", .Platform$dynlib.ext), package = "RTMGreifensee")
+    lib_path <- "your_lib_path"
+    shlib    <- paste0(lib_path, "/RTMGreifensee", .Platform$dynlib.ext)
 
     dyn.unload(shlib)
 
